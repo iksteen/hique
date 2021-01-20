1e03251154b7ed256fcb11175a14f3d416c257e0
Ingmar Steen <iksteen@gmail.com>
2021-01-20 21:31:22 +0100

    Add braces where operator precedence requires it.

--- hique/pgbuilder.py
+++ hique/pgbuilder.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from functools import partial
-from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple
+from typing import Any, Callable, Dict, List, Optional, Sequence, Set, Tuple
 
 from hique.base import FieldAttrDescriptor
 from hique.expr import Expr
@@ -21,6 +21,27 @@ class Args:
 
 
 class PostgresqlQueryBuilder:
+    precedence: List[Set[Optional[str]]] = [
+        {"field", "literal", "arg", "call"},
+        {"pos", "neg"},
+        {"mul", "div", "mod"},
+        {"add", "sub"},
+        {"is_null"},
+        {"is_not_null"},
+        {None},
+        {"lt", "gt", "eq", "le", "ge", "ne"},
+        {"invert"},
+        {"and"},
+        {"or"},
+    ]
+    precedence_map: Dict[Optional[str], int]
+
+    def __init__(self) -> None:
+        self.precedence_map = {}
+        for i, op_names in enumerate(self.precedence):
+            for op_name in op_names:
+                self.precedence_map[op_name] = i
+
     def __call__(self, query: Query) -> Tuple[str, List[Any]]:
         args = Args()
         if isinstance(query, SelectQuery):
@@ -65,6 +86,15 @@ class PostgresqlQueryBuilder:
 
         return f"SELECT {', '.join(values)}{from_}{where}"
 
+    def get_precedence(self, expr: Any) -> int:
+        if not isinstance(expr, Expr):
+            return self.precedence_map["arg"]
+
+        precedence = self.precedence_map.get(expr.op)
+        if precedence is None:
+            return self.precedence_map[None]
+        return precedence
+
     def dot_quote(self, *parts: Optional[str]) -> str:
         return ".".join(f'"{part}"' for part in parts)
 
@@ -102,17 +132,29 @@ class PostgresqlQueryBuilder:
         return f"{f}({', '.join([self.emit(e, args) for e in call_args])})"
 
     def emit_call_expr(self, expr: Expr, args: Args) -> str:
-        schema, name, *call_args = expr.args
+        schema, name, call_args = expr.args
         return self.emit_call(name, call_args, args, schema=schema)
 
-    def emit_infix_op(self, expr: Expr, args: Args, *, infix: str) -> str:
-        return f"({infix.join(self.emit(arg, args) for arg in expr.args)})"
+    def emit_op(
+        self,
+        expr: Expr,
+        args: Args,
+        *,
+        prefix: str = "",
+        infix: str = "",
+        suffix: str = "",
+    ) -> str:
+        precedence = self.get_precedence(expr)
 
-    def emit_prefix_op(self, expr: Expr, args: Args, *, prefix: str) -> str:
-        return f"{prefix}{expr.args[0]}"
+        args_sql = []
+        for arg in expr.args:
+            arg_sql = self.emit(arg, args)
+            arg_precedence = self.get_precedence(arg)
+            if precedence < arg_precedence:
+                arg_sql = f"({arg_sql})"
+            args_sql.append(arg_sql)
 
-    def emit_postfix_op(self, expr: Expr, args: Args, *, postfix: str) -> str:
-        return f"{self.emit(expr.args[0], args)}{postfix}"
+        return f"{prefix}{infix.join(args_sql)}{suffix}"
 
     def not_implemented(self, expr: Expr, args: Args) -> str:
         raise NotImplementedError(expr)
@@ -120,34 +162,34 @@ class PostgresqlQueryBuilder:
     expr_emitter_map: Dict[str, Callable[[PostgresqlQueryBuilder, Expr, Args], str]] = {
         "literal": lambda s, e, a: str(e.args[0]),
         "field": emit_field,
-        "lt": partial(emit_infix_op, infix=" < "),
-        "le": partial(emit_infix_op, infix=" <= "),
-        "eq": partial(emit_infix_op, infix=" = "),
-        "ne": partial(emit_infix_op, infix=" != "),
-        "gt": partial(emit_infix_op, infix=" > "),
-        "ge": partial(emit_infix_op, infix=" >= "),
-        "neg": partial(emit_prefix_op, prefix="-"),
-        "pos": partial(emit_prefix_op, prefix="+"),
+        "lt": partial(emit_op, infix=" < "),
+        "le": partial(emit_op, infix=" <= "),
+        "eq": partial(emit_op, infix=" = "),
+        "ne": partial(emit_op, infix=" <> "),
+        "gt": partial(emit_op, infix=" > "),
+        "ge": partial(emit_op, infix=" >= "),
+        "neg": partial(emit_op, prefix="-"),
+        "pos": partial(emit_op, prefix="+"),
         "abs": lambda self, e, a: self.emit_call("abs", e.args, a),
-        "invert": partial(emit_prefix_op, prefix="NOT "),
-        "add": partial(emit_infix_op, infix=" + "),
-        "sub": partial(emit_infix_op, infix=" - "),
-        "mul": partial(emit_infix_op, infix=" * "),
+        "invert": partial(emit_op, prefix="NOT "),
+        "add": partial(emit_op, infix=" + "),
+        "sub": partial(emit_op, infix=" - "),
+        "mul": partial(emit_op, infix=" * "),
         "matmul": not_implemented,
-        "div": partial(emit_infix_op, infix=" / "),
+        "div": partial(emit_op, infix=" / "),
         "floordiv": lambda self, e, a: self.emit_call("div", e.args, a),
-        "mod": partial(emit_infix_op, infix=" % "),
+        "mod": partial(emit_op, infix=" % "),
         "divmod": not_implemented,
         "pow": lambda self, e, a: self.emit_call("power", e.args, a),
-        "lshift": partial(emit_infix_op, infix=" << "),
-        "rshift": partial(emit_infix_op, infix=" >> "),
-        "and": partial(emit_infix_op, infix=" AND "),
-        "xor": partial(emit_infix_op, infix=" # "),
-        "or": partial(emit_infix_op, infix=" OR "),
+        "lshift": partial(emit_op, infix=" << "),
+        "rshift": partial(emit_op, infix=" >> "),
+        "and": partial(emit_op, infix=" AND "),
+        "xor": partial(emit_op, infix=" # "),
+        "or": partial(emit_op, infix=" OR "),
         "round": lambda self, e, a: self.emit_call("round", e.args, a),
         "floor": lambda self, e, a: self.emit_call("floor", e.args, a),
         "ceil": lambda self, e, a: self.emit_call("ceil", e.args, a),
-        "is_null": partial(emit_postfix_op, postfix=" IS NULL"),
-        "is_not_null": partial(emit_postfix_op, postfix=" IS NOT NULL"),
+        "is_null": partial(emit_op, suffix=" IS NULL"),
+        "is_not_null": partial(emit_op, suffix=" IS NOT NULL"),
         "call": emit_call_expr,
     }
